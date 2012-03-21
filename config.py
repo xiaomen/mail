e852a5fde2770efd67e14d3265563161b0afa039
Menglong Tan <tanmenglong@gmail.com>
2012-03-21 15:31:51 +0800

    add

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; indent-tabs-mode: nil -*-
+#
+# Copyright 2012 crackcell
+#
+
+DEBUG = True
+
+SESSION_KEY = 'xid'
+SESSION_COOKIE_DOMAIN = '.xiaomen.co'
